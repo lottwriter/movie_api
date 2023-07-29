8f80ff9f46fbfac94013ef012b1fc6c8ec256a2c
lettucemayoandonion <lottwriter@gmail.com>
2023-07-28 22:37:11 -0600

    Updated allowed domains

--- index.js
+++ index.js
@@ -192,7 +192,7 @@ startUp();
 
   //CORS implementation
   const cors = require('cors');
-  let allowedOrigins = ['http://localhost:8080', 'http://testsite.com','http://localhost:1234'];
+  let allowedOrigins = ['http://localhost:8080', 'http://testsite.com','http://localhost:1234','https://myflixus.netlify.app'];
 
 app.use(cors({
   origin: (origin, callback) => {
